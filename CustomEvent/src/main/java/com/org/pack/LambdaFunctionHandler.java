a54e6b1ed0159691488add4a3da8fbb48a3af20c
Anupam Biswas <info.anuwas@gmail.com>
2022-12-04 01:29:02 +0530

    firstcommit

---
+++ CustomEvent/src/main/java/com/org/pack/LambdaFunctionHandler.java
@@ -0,0 +1,17 @@
+package com.org.pack;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaFunctionHandler implements RequestHandler<Object, String> {
+
+    @Override
+    public String handleRequest(Object input, Context context) {
+    	context.getLogger().log("CustomEvent lambda is invoked");
+        context.getLogger().log("Input: " + input);
+
+        // TODO: implement your handler
+        return "Hello from Lambda!";
+    }
+
+}
